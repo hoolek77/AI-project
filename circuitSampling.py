559b53dcf6f1ee6fd8edc11c2c8b09ad6201093d
worldwidepaniel <dan.porzucek@gmail.com>
2022-11-22 08:57:44 +0100

    Draft version of script preparing learning sequence

---
+++ circuitSampling.py
@@ -0,0 +1,35 @@
+from motor.py import forward, backward, left, right, stop
+from main.py import makePhoto
+import json
+from time import sleep
+
+sequence = {}
+
+while 1:
+    cmd = input()
+    print(cmd)
+    currentIteration = []
+    currentIteration.append(makePhoto())
+    if(cmd == 'w'):
+        currentIteration.append('w')
+        forward()
+        sleep(1)
+    if(cmd == 's'):
+        currentIteration.append('s')
+        backward()
+        sleep(1)
+    if(cmd == 'a'):
+        currentIteration.append('a')
+        left()
+        sleep(1)
+    if(cmd == 'd'):
+        currentIteration.append('d')
+        right()
+        sleep(1)
+    if(cmd == 'x'):
+        currentIteration.append('x')
+        stop()
+        with open("sequence.json", "w") as outfile:
+          outfile.write(json.dumps(sequence, indent=4))
+    sequence.append[currentIteration]
+    stop()
--- main.py
+++ main.py
@@ -8,16 +8,19 @@ from tts import speak
 camera = PiCamera()
 lastPhotos = []
 
+def makePhoto():
+    fileName = "./" + str(datetime.datetime.now()) + ".jpg"
+    camera.capture(fileName)
+    return fileName
+
 while( True ):
     if(len(lastPhotos) >=10):
         os.remove(lastPhotos[0])
         lastPhotos.pop(0)
     
-    fileName = "./" + str(datetime.datetime.now()) + ".jpg"
-    lastPhotos.append(fileName)
-    camera.capture(fileName)
+    lastPhotos.append(makePhoto())
     
-    result = recognize_img(fileName)
+    result = recognize_img(lastPhotos[-1])
     print(result)
     for res in result:
         speak(res)
