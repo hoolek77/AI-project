2e6879172bead86afb2af1058f9fda36d15e43c9
Michal Zarnecki <michal.zarnecki@companyhouse.de>
2022-11-22 09:47:05 +0100

    improve turns

--- motor.py
+++ motor.py
@@ -39,16 +39,16 @@ def right():
     GPIO.output(IN1, GPIO.LOW)
     GPIO.output(IN2, GPIO.HIGH)
     GPIO.output(ENB, GPIO.HIGH)
-    GPIO.output(IN3, GPIO.LOW)
-    GPIO.output(IN4, GPIO.HIGH)
+    GPIO.output(IN3, GPIO.HIGH)
+    GPIO.output(IN4, GPIO.LOW)
 
 def left():
     GPIO.output(ENB, GPIO.HIGH)
     GPIO.output(IN3, GPIO.LOW)
     GPIO.output(IN4, GPIO.HIGH)
     GPIO.output(ENA, GPIO.HIGH)
-    GPIO.output(IN1, GPIO.LOW)
-    GPIO.output(IN2, GPIO.HIGH)
+    GPIO.output(IN1, GPIO.HIGH)
+    GPIO.output(IN2, GPIO.LOW)
 
 def stop():
     GPIO.output(ENA, GPIO.LOW)
