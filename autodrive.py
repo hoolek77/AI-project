42561f18ecc70c847242ce0c290b59742af01070
Michal Zarnecki <michal.zarnecki@companyhouse.de>
2022-12-31 16:51:01 +0100

    modify tresholds

--- autodrive.py
+++ autodrive.py
@@ -31,12 +31,12 @@ def predict(image)->int:
     Y_pred = model.predict([image])
     for y in Y_pred:
         print(y)
-        if (y < 2.6):
+        if (y < 1.4):
             return 1
-        elif (y >= 2.8 and y < 3.2):
+        elif (y >= 1.4 and y < 2.4):
+            return 2
+        elif (y >= 2.4):
             return 3
-        elif (y >= 3.2110):
-            return 4
         else:
             return int(y)
 
